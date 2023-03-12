126dc710df003791963d48c64b314b039d932f05
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2023-03-11 22:45:27 -0500

    docs: add how-to-compile comment

--- keyboards/crkbd/keymaps/zchsh/keymap.c
+++ keyboards/crkbd/keymaps/zchsh/keymap.c
@@ -16,6 +16,12 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+/*
+To compile:
+qmk compile -kb <keyboard> -km <keymap>
+qmk compile -kb crkbd -km zchsh
+*/
+
 #include QMK_KEYBOARD_H
 #include <stdio.h>
 
