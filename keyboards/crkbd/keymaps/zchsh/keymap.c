679788cc4bd7df0670739a68e21abc83f3a59636
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2025-02-28 15:04:59 -0500

    feat: make arrow keys vim-like

--- keyboards/crkbd/keymaps/zchsh/keymap.c
+++ keyboards/crkbd/keymaps/zchsh/keymap.c
@@ -42,9 +42,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [1] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      KC_TRNS, XXXXXXX, KC_MRWD, KC_MFFD, KC_MPLY, KC_BRIU,                      KC_KB_VOLUME_UP, XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX, KC_TRNS,
+      KC_TRNS, XXXXXXX, KC_MRWD, KC_MFFD, KC_MPLY, KC_BRIU,                      KC_KB_VOLUME_UP, KC_KB_VOLUME_DOWN, KC_NO, KC_NO, KC_NO, KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TRNS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BRID,                      KC_KB_VOLUME_DOWN, KC_LEFT, KC_DOWN, KC_RIGHT,XXXXXXX, KC_TRNS,
+      KC_TRNS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BRID,                      KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT,XXXXXXX, KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_TRNS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_MUTE, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_TRNS,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
